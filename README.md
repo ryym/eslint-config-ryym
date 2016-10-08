e641779ce2a69ba6f0c8d8bdc7a67d285ad1b792
ryym <ryym.64@gmail.com>
2016-10-09 02:07:24 +0900

    Add config file for React JSX

--- README.md
+++ README.md
@@ -3,3 +3,31 @@
 * [ESLint](https://github.com/eslint/eslint)
 * [ESLint shareable configs](http://eslint.org/docs/developer-guide/shareable-configs)
 
+## Usage
+
+First, install ESLint and this config via npm.
+
+```sh
+npm i -D eslint eslint-config-ryym
+```
+
+### Use basic configs
+
+```json
+module.exports = {
+  "extends": "ryym/base"
+}
+```
+
+### Use basic configs for [React](https://facebook.github.io/react/) (JSX)
+
+```json
+module.exports = {
+  "extends": "ryym/base-react",
+  "plugins": [
+    "react"
+  ]
+}
+```
+
+Note that you need to install [eslint-plugin-react](https://github.com/yannickcr/eslint-plugin-react).
---
+++ base-react.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "extends": "ryym/base",
+
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true,
+    }
+  },
+
+  "rules": {
+    "jsx-quotes": [2, "prefer-double"],
+    "react/jsx-closing-bracket-location": 2,
+    "react/jsx-curly-spacing": [2, "never"],
+    "react/jsx-equals-spacing": [2, "never"],
+    "react/jsx-indent": [2, 2],
+    "react/jsx-indent-props": [1, 2],
+    "react/jsx-key": 1,
+    "react/jsx-no-bind": 1,
+    "react/jsx-no-duplicate-props": 2,
+    "react/jsx-no-undef": 2,
+    "react/jsx-uses-react": 1,
+    "react/jsx-uses-vars": 2,
+    "react/no-deprecated": 2,
+    "react/no-direct-mutation-state": 2,
+    "react/self-closing-comp": 1,
+    "react/wrap-multilines": 2
+  }
+}
